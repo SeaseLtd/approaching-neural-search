4ff6434e7bcad5df5d3ba0edf773fbb1d21d90b4
Ilaria <petreti.ilaria@gmail.com>
2023-03-15 15:35:40 +0300

    Added some comments

--- from-text-to-vectors/single-sentence-transformers.py
+++ from-text-to-vectors/single-sentence-transformers.py
@@ -1,9 +1,14 @@
 from sentence_transformers import SentenceTransformer
 
+# A sentence to encode
 sentence = ["what is the gre test"]
 
+# Load or create a SentenceTransformer model
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+# Compute sentence embeddings
 embeddings = model.encode(sentence)
-# to remove the initial "array" string and the trailing "dtype=float32"
-vector = repr(list(embeddings)[0])[6:-22]
-print(vector)
+
+# Create an array of floats comma separated (removing the initial "array" string and the trailing "dtype=float32")
+vector_embeddings = repr(list(embeddings)[0])[6:-22]
+print(vector_embeddings)

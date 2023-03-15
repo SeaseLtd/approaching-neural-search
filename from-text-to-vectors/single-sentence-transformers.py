a89781ce84874feeb5a8d11099326c608252f708
Ilaria <petreti.ilaria@gmail.com>
2023-03-15 07:47:44 +0300

    To print the vector as a string

--- from-text-to-vectors/single-sentence-transformers.py
+++ from-text-to-vectors/single-sentence-transformers.py
@@ -1,7 +1,9 @@
 from sentence_transformers import SentenceTransformer
-sentences = ["what is the name of the famous painting at louvre"]
+
+sentence = ["what is the gre test"]
 
 model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-embeddings = model.encode(sentences)
-vector = list(embeddings)
+embeddings = model.encode(sentence)
+# to remove the initial "array" string and the trailing "dtype=float32"
+vector = repr(list(embeddings)[0])[6:-22]
 print(vector)
